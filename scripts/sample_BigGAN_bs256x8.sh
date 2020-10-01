0ff4fae9524712fda83c16feac0564073e5074e9
niviruwijayaratne <nnw2@illinois.edu>
2020-09-30 19:09:58 -0500

    conditional generation and interpolation

--- scripts/sample_BigGAN_bs256x8.sh
+++ scripts/sample_BigGAN_bs256x8.sh
@@ -15,9 +15,11 @@ python sample.py \
 --hier --dim_z 120 --shared_dim 128 \
 --ema --ema_start 20000 \
 --use_multiepoch_sampler \
---samples_root samples \
---sample_num_npz 20000 \
+--samples_root samples2 \
+--sample_num_npz 5000 \
+--sample_sheets \
+--sample_interps \
+--sample_sheet_folder_num 1 \
 --experiment_name BigGan1 \
 --test_every 2000 --save_every 1000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
 --skip_init --G_batch_size 8  --use_ema --G_eval_mode --sample_trunc_curves 0.05_0.05_1.0  \
---sample_inception_metrics --sample_npz  --sample_random --sample_sheets --sample_interps 
